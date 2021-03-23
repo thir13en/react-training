2ac57afc868330bc2924a433a4ae8b508b029312
santiloopz <santilpzate@gmail.com>
2021-03-23 08:36:51 +0100

    Add Burger creator and TODO for atomic styles

--- exercises/janburger-crieitor/src/App.tsx
+++ exercises/janburger-crieitor/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+
+// TODO: add atomic styles
 const App = () => (
 	<div>
 		<Layout>
-			<p>Test</p>
+			<BurgerBuilder />
 		</Layout>
 	</div>
 );
---
+++ exercises/janburger-crieitor/src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BurgerBuilder extends Component {
+	render() {
+		return (
+			<>
+				<div>Burger</div>
+				<div>Build Controls</div>
+			</>
+		);
+	}
+}
